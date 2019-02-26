239c2a297d0911fb42426bfbad8031ec32875dad
Pisanina <mailszarookiego@o2.pl>
2019-02-26 19:58:35 +0100

    Task71: Repered

--- CodeTogetherNG_DB/Stored Procedures/Project_Add.sql
+++ CodeTogetherNG_DB/Stored Procedures/Project_Add.sql
@@ -1,11 +1,14 @@
-﻿ CREATE PROCEDURE [dbo].[Project_Add] @Title Nvarchar(50), @Description Nvarchar(1000),
- @TechList TechnologyList readonly
+﻿
+ Create PROCEDURE [dbo].[Project_Add] @Title Nvarchar(50), @Description Nvarchar(1000),
+ @TechList TechnologyList readonly, @UserName Nvarchar(256)
   AS
+	Declare @UserId Nvarchar(256)
+	Set @UserId = (Select Id from AspNetUsers Where UserName = @UserName)
     IF Exists (Select 1 from @TechList)
     begin
 		begin Try
 			 BEGIN TRANSACTION
-				Insert into Project (Title, Description) Values ( @Title,  @Description);
+				Insert into Project (Title, Description, OwnerId) Values ( @Title,  @Description, @UserId);
   
 				Insert into ProjectTechnology (ProjectId, TechnologyId) Select SCOPE_IDENTITY(), Id from @TechList
 			COMMIT TRANSACTION
@@ -16,8 +19,7 @@
 	end Catch
   end
   ELSE 
-  Insert into Project (Title, Description) Values ( @Title,  @Description)
-GO
+  Insert into Project (Title, Description, OwnerId) Values ( @Title,  @Description, @UserId)
 GRANT EXECUTE
     ON OBJECT::[dbo].[Project_Add] TO [codetogetherng]
     AS [dbo];
