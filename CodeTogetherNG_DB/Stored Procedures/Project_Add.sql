5e6312b05e3befaa8c3a342e039d48cb82c26109
Pisanina <mailszarookiego@o2.pl>
2019-03-01 10:59:44 +0100

    Task68: Add a new field on creation of project: Looking for new members? Saving the selected value to DB is a part of this task

--- CodeTogetherNG_DB/Stored Procedures/Project_Add.sql
+++ CodeTogetherNG_DB/Stored Procedures/Project_Add.sql
@@ -1,6 +1,6 @@
 ﻿
  Create PROCEDURE [dbo].[Project_Add] @Title Nvarchar(50), @Description Nvarchar(1000),
- @TechList TechnologyList readonly, @UserName Nvarchar(256)
+ @TechList TechnologyList readonly, @UserName Nvarchar(256), @NewMembers Bit
   AS
 	Declare @UserId Nvarchar(450)
 	Set @UserId = (Select Id from AspNetUsers Where UserName = @UserName)
@@ -8,7 +8,7 @@
     begin
 		begin Try
 			 BEGIN TRANSACTION
-				Insert into Project (Title, Description, OwnerId) Values ( @Title,  @Description, @UserId);
+				Insert into Project (Title, Description, OwnerId, NewMembers) Values ( @Title,  @Description, @UserId, @NewMembers);
   
 				Insert into ProjectTechnology (ProjectId, TechnologyId) Select SCOPE_IDENTITY(), Id from @TechList
 			COMMIT TRANSACTION
@@ -19,9 +19,9 @@
 	end Catch
   end
   ELSE 
-  Insert into Project (Title, Description, OwnerId) Values ( @Title,  @Description, @UserId)
-GO
+  Insert into Project (Title, Description, OwnerId, NewMembers) Values ( @Title,  @Description, @UserId, @NewMembers)
 
+GO
 GRANT EXECUTE
     ON OBJECT::[dbo].[Project_Add] TO [codetogetherng]
 GO
