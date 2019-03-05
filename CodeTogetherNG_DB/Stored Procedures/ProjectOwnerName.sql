dff0a4910fed8abfda680df08d6882aed950c89b
Pisanina <mailszarookiego@o2.pl>
2019-03-05 17:50:59 +0100

    Task76: Projects should be editable by its owner

---
+++ CodeTogetherNG_DB/Stored Procedures/ProjectOwnerName.sql
@@ -0,0 +1,6 @@
+﻿  Create Procedure ProjectOwnerName @Id int
+  AS
+  Select UserName From AspNetUsers inner Join Project On AspNetUsers.Id = Project.OwnerId  where Project.ID=@Id
+
+  Go
+  Grant EXECUTE ON ProjectOwnerName to codetogetherng
---
+++ CodeTogetherNG_DB/Stored Procedures/Project_Edit.sql
@@ -0,0 +1,23 @@
+﻿ Create PROCEDURE [dbo].[Project_Edit] @Id int, @Title Nvarchar(50), @Description Nvarchar(1000),
+ @TechList TechnologyList readonly, @NewMembers bit
+ AS
+  begin
+		begin Try
+			 BEGIN TRANSACTION
+				Update Project SET Title = @Title, Description = @Description, 
+						NewMembers = @NewMembers
+						where Id=@Id
+				Delete From ProjectTechnology where ProjectId = @Id
+				Insert Into ProjectTechnology (ProjectId, TechnologyId) Select @Id, Id from @TechList
+			COMMIT TRANSACTION
+		end Try
+ 	begin Catch
+		ROLLBACK Transaction
+		throw
+	end Catch
+  end
+
+GO
+GRANT EXECUTE
+    ON OBJECT::[dbo].[Project_Edit] TO [codetogetherng]
+    AS [dbo];
