4465086092cf43a95101a13f508d55beeb887545
Pisanina <mailszarookiego@o2.pl>
2019-03-09 14:05:15 +0100

    Task97: A logged user should be able to send a request to join a project if the project looks for members

---
+++ CodeTogetherNG_DB/Stored Procedures/ProjectMember_NewRequest.sql
@@ -0,0 +1,7 @@
+﻿Create Procedure ProjectMember_NewRequest  
+@ProjectId int, @MemberId Nvarchar(450), @Message nvarchar(500) 
+AS
+Insert Into ProjectMember (ProjectId, MemberId, [Message]) values (@ProjectId, @MemberId, @Message);
+
+Go
+Grant EXECUTE ON ProjectMember_NewRequest to codetogetherng
---
+++ CodeTogetherNG_DB/Tables/ProjectMember.sql
@@ -0,0 +1,6 @@
+﻿Create Table ProjectMember
+(Id int not null primary key identity(1,1), 
+	ProjectId int FOREIGN KEY REFERENCES Project(ID) not null,
+	MemberId Nvarchar(450) not null constraint FK_ProjectMember_AspNetUsers FOREIGN KEY REFERENCES AspNetUsers(id), 
+	MessageDate datetimeOffset not null constraint DF_ProjectMember_MessageDate default  (sysdatetimeoffset()), 
+	[Message] NVarchar(500) not null);
