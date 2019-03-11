c574d94e30012b7cad7750dffde302fbf7d651f4
Pisanina <mailszarookiego@o2.pl>
2019-03-11 17:23:55 +0100

    Task98: Repeired Task100: Repaired

--- CodeTogetherNG_DB/Stored Procedures/RequestsCount.sql
+++ CodeTogetherNG_DB/Stored Procedures/RequestsCount.sql
@@ -1,6 +1,6 @@
 ﻿Create Procedure RequestsCount @ProjectId int
-As
-Select count(ProjectId) from ProjectMember where ProjectId = @ProjectId;
+   As
+   Select count(ProjectId) from ProjectMember where ProjectId = @ProjectId AND AddMember Is Null;
 
 Go
 Grant EXECUTE ON RequestsCount to codetogetherng
--- CodeTogetherNG_DB/Stored Procedures/RequestsList.sql
+++ CodeTogetherNG_DB/Stored Procedures/RequestsList.sql
@@ -1,7 +1,7 @@
 ﻿CREATE Procedure RequestsList @ProjectId int
 	 AS
 	 Select UserName, [Message], MemberId, Title, @ProjectId AS ProjectId,
-	 FORMAT(MessageDate, 'dd/MM/yyyy') AS MessageDate
+	 FORMAT(MessageDate, 'dd/MM/yyyy') AS MessageDate, AddMember AS Accept
 	 From ProjectMember
 	 Join AspNetUsers On (ProjectMember.MemberId=AspNetUsers.Id)
 	 Join Project On (ProjectMember.ProjectId=Project.ID)
