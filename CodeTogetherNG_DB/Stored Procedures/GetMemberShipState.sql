50f07a3c5334a396a33ce100b8ff26c6d91a9f9a
Pisanina <mailszarookiego@o2.pl>
2019-03-12 19:25:54 +0100

    Task115:  If a user is already a member of a project, He should not be able to send a new join request Task116:  if a user was rejected to join a project, he should not be able to send another request to this project within 30 days from rejection. Task117:  If a user sent a join request and it is still pending, he should not be able to send another one.

---
+++ CodeTogetherNG_DB/Stored Procedures/GetMemberShipState.sql
@@ -0,0 +1,9 @@
+﻿Create Procedure GetMemberShipState @ProjectId int, @UserName nvarchar(450)
+As
+Declare @MemberId Nvarchar(450)
+	Set @MemberId = (Select Id from AspNetUsers Where UserName = @UserName)
+Select MessageDate, AddMember From ProjectMember
+Where ProjectId= @ProjectId AND MemberID =@MemberId
+
+GO
+Grant EXECUTE ON GetMemberShipState to codetogetherng
