51cff628e15432601895ad788d870fc47258577e
Pisanina <mailszarookiego@o2.pl>
2019-03-12 13:13:42 +0100

    Task101: List of members should be displayed on a derail view of the project

---
+++ CodeTogetherNG_DB/Stored Procedures/MembersList.sql
@@ -0,0 +1,8 @@
+﻿ Create Procedure MembersList @ProjectId int
+   As
+   Select UserName From AspNetUsers
+   Join  ProjectMember On ProjectMember.MemberId = AspNetUsers.Id
+   where ProjectId = @ProjectId And AddMember = 1
+
+   Go
+     Grant EXECUTE ON MembersList to codetogetherng
