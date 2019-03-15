66097c0ffce6ec5eb1ecfdb294bb63a17b475d5c
Pisanina <mailszarookiego@o2.pl>
2019-03-15 21:42:41 +0100

    Task103: A user should be able to specify techs he knows and level of expertise in a scale of 5. Repair

--- CodeTogetherNG_DB/Stored Procedures/TechnologyLevel_Add.sql
+++ CodeTogetherNG_DB/Stored Procedures/TechnologyLevel_Add.sql
@@ -1,8 +1,16 @@
-﻿Create Procedure TechnologyLevel_Add  @UserName nvarchar(256), @TechnologyId int, @TechLevel int
+﻿Create  Procedure [dbo].[TechnologyLevel_Add]  @UserName nvarchar(256), @TechnologyId int, @TechLevel int
 AS
 Declare @UserId nvarchar(450)
 SET @UserId = (Select Id from AspNetUsers Where UserName = @UserName)
-Insert Into UserTechnologyLevel (UserId, TechnologyId, TechLevel) 
-Values (@UserId, @TechnologyId, @TechLevel)
+IF EXISTS(Select 1 From UserTechnologyLevel Where TechnologyId = @TechnologyId AND UserId= @UserId)
+begin
+ Update UserTechnologyLevel SET TechLevel = @TechLevel Where TechnologyId = @TechnologyId AND UserId= @UserId
+end
+
+else
+begin
+	Insert Into UserTechnologyLevel (UserId, TechnologyId, TechLevel) 
+	Values (@UserId, @TechnologyId, @TechLevel)
+end
 Go
   Grant EXECUTE ON  TechnologyLevel_Add to codetogetherng  
--- CodeTogetherNG_DB/Stored Procedures/TechnologyLevel_Change.sql
+++
@@ -1,8 +0,0 @@
-﻿Create Procedure TechnologyLevel_Change @Id int, @TechLevel int
-AS
-Update UserTechnologyLevel 
-SET TechLevel = @TechLevel
-Where Id = @Id
-
-Go
-  Grant EXECUTE ON  TechnologyLevel_Change to codetogetherng  
