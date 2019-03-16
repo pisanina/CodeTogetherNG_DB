2db6e72f6b10de40d257fecd5871def55780082e
Pisanina <mailszarookiego@o2.pl>
2019-03-16 16:46:14 +0100

    Task121: Add a list of projects on user profile screen.

---
+++ CodeTogetherNG_DB/Stored Procedures/GetProjectTitleForUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE Procedure GetProjectTitleForUser @UserName nvarchar(256)
+ AS
+ Declare @UserId nvarchar(450)
+SET @UserId = (Select Id from AspNetUsers Where UserName = @UserName)
+  Select ID, Title from Project Where ID In
+ ( Select ID from Project 
+  where OwnerId = @UserId
+  Union
+  Select ProjectId from ProjectMember
+  where MemberId = @UserId AND ADDMember=1)
+
+  Go 
+   Grant EXECUTE ON GetProjectTitleForUser to codetogetherng  
+ 
