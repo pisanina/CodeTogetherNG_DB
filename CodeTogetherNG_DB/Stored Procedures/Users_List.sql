1c8503b22ee1faff295af4970c865d8edb663b24
Pisanina <mailszarookiego@o2.pl>
2019-03-23 12:16:30 +0100

    Task123: Add a count of projects as an owner to the list of users screen Task124: add a  count of projects a user is member of on the list of users screen Task124: add counts for each level of skills on the list of users screen

--- CodeTogetherNG_DB/Stored Procedures/Users_List.sql
+++ CodeTogetherNG_DB/Stored Procedures/Users_List.sql
@@ -1,6 +1,31 @@
 ﻿Create Procedure Users_List
 AS
-Select UserName From AspNetUsers
+SELECT AllUsers.UserName, IsNull(OwnerOf, 0) AS OwnerOf, IsNull(MemberOf,0) As MemberOf, 
+		IsNULL(BeginnerIn, 0) AS BegginerIn, IsNull(AdvancedIn, 0) AS AdvancedIn, IsNull(ExpertIn,0 ) AS ExpertIn FROM 
+( Select UserName From AspNetUsers ) As AllUsers
+	Left Join
+	(
+		Select UserName, Count(OwnerId) AS OwnerOf 
+			From AspNetUsers
+				Join Project On AspNetUsers.Id = Project.OwnerId Group by UserName
+	) AS OwnerCount ON AllUsers.UserName = OwnerCount.UserName
+	Left Join
+	(
+		Select UserName, Count(case when addmember=1 then 1 else NULL end) AS MemberOf
+			From AspNetUsers 
+				Join ProjectMember On AspNetUsers.Id = ProjectMember.MemberId
+			Group By UserName
+	) As MemberCount On OwnerCount.UserName = MemberCount.UserName
+	Left Join 
+	(
+		Select UserName
+			, Count(case when TechLevel=1 then 1 else Null end) AS BeginnerIn
+			, Count(case when TechLevel=2 then 1 else Null end) AS AdvancedIn
+			, Count(case when TechLevel=3 then 1 else Null end) AS ExpertIn
+			From AspNetUsers
+				Left Join UserTechnologyLevel ON AspNetUsers.Id = UserTechnologyLevel.UserId
+			Group by UserName 
+	) AS skillLevels ON  OwnerCount.UserName=skillLevels.UserName 
 
 GO
   Grant EXECUTE ON Users_List to codetogetherng 
