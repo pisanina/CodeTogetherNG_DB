e114904bfd13a048ff3c861528e630d78ca89ac6
Pisanina <mailszarookiego@o2.pl>
2019-04-09 14:57:12 +0200

    Repeir UserTechnologyLevel

--- CodeTogetherNG_DB/Tables/UserTechnologyLevel.sql
+++ CodeTogetherNG_DB/Tables/UserTechnologyLevel.sql
@@ -1,7 +1,7 @@
 ﻿Create Table UserTechnologyLevel 
 (Id int not null identity (1,1)
-, UserId nvarchar(450) constraint FK_UserTechnologyLevel_AspNetUsers FOREIGN KEY REFERENCES AspNetUsers(id)
-, TechnologyId int constraint FK_UserTechnologyLevel_Technology FOREIGN KEY REFERENCES Technology(id)
+, UserId nvarchar(450) constraint FK_UserTechnologyLevel_AspNetUsers FOREIGN KEY REFERENCES AspNetUsers(id) NOT NULL
+, TechnologyId int constraint FK_UserTechnologyLevel_Technology FOREIGN KEY REFERENCES Technology(id) NOT NULL
 , TechLevel int not null
 , CONSTRAINT UC_UserTechnologyLevel_UserId_TechnologyId UNIQUE (UserId, TechnologyId))
 GO
